f95d7511bf11239bc564541a29007962d89f1657
ErickaBermudez <37844174+ErickaBermudez@users.noreply.github.com>
2019-04-17 20:33:32 -0600

    Improving structure of javascripts :art:

---
+++ javascripts/main.js
@@ -0,0 +1,20 @@
+//nav fade
+
+$(document).ready(function(){
+    var $nav = $('#nav-bar');//Caching element
+    // hide .navbar first - you can also do this in css .nav{display:none;}
+    $nav.hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+            // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 100) { //For dynamic effect use $nav.height() instead of '100'
+                $nav.fadeIn();
+            } else {
+                $nav.fadeOut();
+            }
+        });
+    });
+
+});
